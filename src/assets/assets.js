c7fe078e7442af5764477ec33d4172a95c6b4dd0
Pawan Tripathi <165460080+pawan8705@users.noreply.github.com>
2026-02-21 11:59:52 +0530

    Update assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -63,7 +63,7 @@ import earning_icon from './earning_icon.svg'
 import patients_icon from './patients_icon.svg'
 
 export const assets = {
-    appointment_img, header_img, group_profiles, logo, chats_icon,
+    appointment_img, group_profiles, logo, chats_icon,
     verified_icon, info_icon, arrow_icon, contact_image, about_image,
     menu_icon, cross_icon, dropdown_icon, upload_icon, add_icon,
     admin_logo, appointment_icon, cancel_icon, doctor_icon, upload_area,
